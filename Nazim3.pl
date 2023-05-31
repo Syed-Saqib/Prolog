59622942b3b9741ed6f145b4a226900df821c4a7
Mudit Gupta <muditg.world@gmail.com>
2023-05-31 20:02:41 +0530

    Prolog codes

--- Nazim3.pl
+++
@@ -1,4 +0,0 @@
-c_to_f(C,F) :-
-F is C * 9 / 5 + 32.
-freezing(F) :-
-F =< 32.
---
+++ Program2_Simple_Facts.pl
@@ -0,0 +1,12 @@
+likes(mudit,play).
+boy(mudit).
+eats(nazim,biryani).
+owns(mudit,laptop).
+brothers(mudit,syed).
+cat(john).
+loves_to_play(mudit,valorant).
+of_color(hair,black).
+loves_to_watch_movies(rishabh).
+lazy(john).
+happy(rishabh).
+
